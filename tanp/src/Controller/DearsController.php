10b64da2a87cb9c0b56af6a310a1d4d0330cfd4c
ippeikamimura <ippei_kamimura@icloud.com>
2021-03-17 16:42:59 +0900

    added edit dears api

--- tanp/src/Controller/DearsController.php
+++ tanp/src/Controller/DearsController.php
@@ -58,18 +58,73 @@ class DearsController extends AppController
         }
     }
 
+    public function edit($id = null)
+    {
+        $request = $this->request->getData();
+        $dear = $this->Dears->get($id, [
+            'contain' => []
+        ]);
+        if ($this->request->is(['put'])) {
+            $dear = $this->Dears->patchEntity($dear, [
+                'name' => $request['name'],
+                'gender' => $request['gender'],
+                'age' => $request['age'],
+                'segment' => $request['segment'],
+                'user_id' => $this->Auth->user('id'),
+            ]);
+            if ($this->Dears->save($dear)) {
+                $this->set([
+                    'success' => true,
+                    'dear' => $dear,
+                    '_serialize' => ['success', 'dear']
+                ]);
+            } else {
+                $this->response->statusCode(400);
+                $errors = $dear->errors();
+                $this->set([
+                    'success' => false,
+                    'dear' => $dear,
+                    'errors' => $errors,
+                    '_serialize' => ['success', 'dear', 'errors']
+                ]);
+            }
+        }
+    }
+
     public function list()
     {
         if ($this->request->is('get')) {
             $userId = $this->Auth->user('id');
             $dears = TableRegistry::getTableLocator()->get('Dears');
-            $query = $dears->find()->contain(['Anniversaries'])->where(['user_id' => $userId]);
+            $query = $dears->find()->contain(['Anniversaries'])->where(['user_id' => $userId])->order(['id' => 'desc']);
             $paginate = $this->paginate($query);
             $this->set([
                 'data' => $paginate,
                 'pagination' => $this->request->param('paging')['Dears'],
                 '_serialize' => ['data', 'pagination']
             ]);
+        }
+    }
+
+    public function delete($id = null)
+    {
+        $this->request->allowMethod(['delete']);
+        $dear = $this->Dears->get($id);
+        if ($this->Dears->delete($dear)) {
+            $this->set([
+                'success' => true,
+                'dear' => $dear,
+                '_serialize' => ['success', 'dear']
+            ]);
+        } else {
+            $this->response->statusCode(400);
+                $errors = $dear->errors();
+                $this->set([
+                    'success' => false,
+                    'dear' => $dear,
+                    'errors' => $errors,
+                    '_serialize' => ['success', 'dear', 'errors']
+                ]);
         }
     }
 }
