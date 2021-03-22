378d61e06119e18fdb1148e1cc3a721b04c4892e
ippeikamimura <ippei_kamimura@icloud.com>
2021-03-23 07:53:50 +0900

    記念日一覧テストコード追加

--- tanp/tests/TestCase/Repository/AnniversariesRepositoryTest.php
+++ tanp/tests/TestCase/Repository/AnniversariesRepositoryTest.php
@@ -91,4 +91,90 @@ class AnniversariesRepositoryTest extends TestCase
             ],
         ];
     }
+
+    public function testDeleteAnniversary(): void
+    {
+        $isDelete = $this->anniversariesRepository->deleteAnniversary(1);
+        $this->assertTrue($isDelete);
+    }
+
+    public function testListByDearId():void
+    {
+        $result = $this->anniversariesRepository->listByDearId(1);
+        foreach ($result as $anniversary) {
+            $this->assertSame(1, $anniversary->dear_id);
+        }
+    }
+
+    /**
+     * @dataProvider getTestCaseForGetSortMonthByUserId
+     *
+     * @param integer $startMonth
+     * @param array $expected
+     * @return void
+     */
+    public function testGetSortMonthByUserId(int $startMonth, array $expected): void
+    {
+        $result = $this->anniversariesRepository->getSortMonthByUserId(1, $startMonth);
+        $aggregation = [];
+        foreach ($result as $anniversary) {
+            @$aggregation[$anniversary['month']] += 1;
+        }
+
+        $this->assertSame($expected, array_keys($aggregation));
+    }
+
+    public function getTestCaseForGetSortMonthByUserId(): array
+    {
+        return [
+            '1月はじまり' => [
+                'startMonth' => 1,
+                'expected' => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, ],
+            ],
+            '2月はじまり' => [
+                'startMonth' => 2,
+                'expected' => [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, ],
+            ],
+            '3月はじまり' => [
+                'startMonth' => 3,
+                'expected' => [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, ],
+            ],
+            '4月はじまり' => [
+                'startMonth' => 4,
+                'expected' => [4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, ],
+            ],
+            '5月はじまり' => [
+                'startMonth' => 5,
+                'expected' => [5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, ],
+            ],
+            '6月はじまり' => [
+                'startMonth' => 6,
+                'expected' => [6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, ],
+            ],
+            '7月はじまり' => [
+                'startMonth' => 7,
+                'expected' => [7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, ],
+            ],
+            '8月はじまり' => [
+                'startMonth' => 8,
+                'expected' => [8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, ],
+            ],
+            '9月はじまり' => [
+                'startMonth' => 9,
+                'expected' => [9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, ],
+            ],
+            '10月はじまり' => [
+                'startMonth' => 10,
+                'expected' => [10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, ],
+            ],
+            '11月はじまり' => [
+                'startMonth' => 11,
+                'expected' => [11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ],
+            ],
+            '12月はじまり' => [
+                'startMonth' => 12,
+                'expected' => [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ],
+            ],
+        ];
+    }
 }
